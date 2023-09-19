9213718f602db462c1f69bd27a8c5588e7428cc7
Gabriel Rocha <gabriel.rocha20@live.com>
2023-09-19 16:03:50 -0300

    created the static file and partials

--- precificacao/urls.py
+++ precificacao/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from precificacao.views import index
+from precificacao.views import index, cadastro_receita
 
 urlpatterns = [
-  path('', index)
+  path('', index, name = 'index'),
+  path('cadastro-receita', cadastro_receita, name = 'cadastro_receita')
 ]
--- precificacao/views.py
+++ precificacao/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
   return render(request, 'precificacao/index.html')
+
+def cadastro_receita(request):
+  return render(request, 'precificacao/cadastro-receita.html')
